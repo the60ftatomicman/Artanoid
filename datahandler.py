08bdc9c2d40f9a8345112bfb6b11f188c44c6c63
the60ftatomicman <garb7477@gmail.com>
2024-03-18 21:54:00 -0400

    v1.0.0 sorted a bit of the save stuff to be better. slowly dismantelling the big data blob.

--- datahandler.py
+++ datahandler.py
@@ -1,5 +1,14 @@
-from os import path
+
+
+# ---------- Imports
+# ----- Internal Libs
 from enum import Enum
+from math import floor
+from os import path
+# ----- External Libs
+# ----- User Generated Libs
+import blocks
+
 dirRoot    = path.dirname(path.realpath(__file__))
 dirData    = dirRoot+"\\data"
 
@@ -15,11 +24,13 @@ class BlockWriteDirection(Enum):
 # Checksums -> any overrides we need to provide for the game to run
 # --
 class ROMWriter:
-    def __init__(self,game,roms,memoffsets,checksums):
+    def __init__(self,game,roms,memoffsets,checksums,direction,blockset):
         self.game       = game
         self.roms       = roms
         self.memoffsets = memoffsets
         self.checksums  = checksums
+        self.direction  = direction
+        self.blockset   = blockset
     ##---
     ## Functions Saving
     ##--
@@ -46,6 +57,53 @@ class ROMWriter:
                 return self.checksums[writeMemoryLocation]
         return currentData
 
+    def save(self,lvlStart,rows,cols,lstBrickData):
+        content     = []
+        romFilePath = self.getROMFilePath(lvlStart)
+        #now adjust lvlStart!
+        lvlStart    = self.adjustROMAddressForLvlStart(lvlStart)
+        lvlStart    = int(lvlStart)
+        lvlEnd      = lvlStart+(cols * rows)
+        romFilePath = self.getROMFilePath(lvlStart)
+
+        print("Reading Rom: %s" % romFilePath)
+        with open(romFilePath, "rb") as fr:
+            data = fr.read(1)
+            content=data
+            idx=0
+            while data:
+                data = fr.read(1)
+                idx+=1
+                checksumData = self.checkIfChecksum(idx,data)
+                if checksumData != data:
+                    content+=checksumData
+                # TODO -- this is shifted in the dumbest way and I cannot figure it out.
+                elif idx >= lvlStart and idx < lvlEnd:
+                    offset  = (idx-lvlStart)
+                    cellRow = floor(offset/cols)
+                    cellCol = floor(offset%cols)
+                    cellIdx = (cellRow*cols)+(cellCol)
+                    if self.direction == BlockWriteDirection.RIGHT_TO_LEFT:
+                        cellIdx = (cellRow*cols)+(cols-cellCol)-1 # Because taito wanted to throw off hackers?
+                    #print("Mem Idx: %d Cell Idx: %d, Row: %d Col: %d" % (offset,cellIdx,cellRow,cellCol))
+                    #try:
+                    romCode  = self.blockset[lstBrickData[cellIdx]].getCode_Static()
+                    content += romCode
+                    #except:
+                    #    print("ERROR!"")
+                    #    content+=data
+                else:
+                    content+=data
+
+        print("Done Reading Rom")
+        print("Writing Rom: %s" % romFilePath)
+        if ".new" not in romFilePath: 
+            with open(romFilePath+".new", "wb") as fw:
+                fw.write(content)
+        else:
+            with open(romFilePath, "wb") as fw:
+                fw.write(content)
+        print("Done Writing Rom")
 #---
 #
 # Inherited Classes
@@ -54,9 +112,81 @@ class ROMWriter:
 class ROMWriter_Arkanoid(ROMWriter):
     def __init__(self):
         super().__init__("arkanoid",
-        ["a75-18.ic16","a75-19.ic17"],[0x0000,0x8000],{})
+        ["a75-18.ic16","a75-19.ic17"],
+        [0x0000,0x8000],{},
+        BlockWriteDirection.LEFT_TO_RIGHT,
+        {
+            "clear"          : blocks.Clear(),
+            "white_flat"     : blocks.White_Flat(),
+            "red_flat"       : blocks.Red_Flat(),
+            "pink_flat"      : blocks.Pink_Flat(),
+            "orange_flat"    : blocks.Orange_Flat(),
+            "yellow_flat"    : blocks.Yellow_Flat(),
+            "green_flat"     : blocks.Green_Flat(),
+            "turqouise_flat" : blocks.Turqouise_Flat(),
+            "blue_flat"      : blocks.Blue_Flat(),
+            "grey_flat"      : blocks.Grey_Flat(),
+            "gold_flat"      : blocks.Gold_Flat(),
+        }
+    )
 
 class ROMWriter_ArkanoidRevengeOfDoh(ROMWriter):
     def __init__(self):
-        super().__init__("arkanoidrevengeofdoh",
-        ["b08_13.3e"],[0x0000],{int(0x0A99):b'\xAF'})
+        super().__init__(
+        "arkanoidrevengeofdoh",
+        ["b08_13.3e"],[0x0000],{int(0x0A99):b'\xAF'},
+        BlockWriteDirection.RIGHT_TO_LEFT,
+        {
+            "clear"          : blocks.Clear(),
+            "white_flat"     : blocks.White_Flat_AROD(),
+            "red_flat"       : blocks.Red_Flat_AROD(),
+            "pink_flat"      : blocks.Pink_Flat_AROD(),
+            "orange_flat"    : blocks.Orange_Flat_AROD(),
+            "yellow_flat"    : blocks.Yellow_Flat_AROD(),
+            "green_flat"     : blocks.Green_Flat_AROD(),
+            "turqouise_flat" : blocks.Turqouise_Flat_AROD(),
+            "blue_flat"      : blocks.Blue_Flat_AROD(),
+            "grey_1ridge"    : blocks.Grey_1Ridge_AROD(),
+            "gold_1ridge"    : blocks.Gold_1Ridge_AROD(),
+            "grey_2ridge"    : blocks.Grey_2Ridge_AROD()
+        }
+    )
+#---
+#
+# The big config
+#
+#---
+GAME_DATA = {
+    "arkanoid":{
+        "ROWS":18,
+        "COLS":13,
+        "DATAHANDLER": ROMWriter_Arkanoid(),
+        "LEVELS":{
+            "1":0xBF15,"2":0x0138,"3":0xBE2B,"4":0x04CB,"5":0x0FE0,
+            "6":0x14D8,"7":0x16C1,"8":0x1F46,"9":0x22CE,"10":0x2980,
+            "11":0x0A08,"12":0x3460,"13":0x40F6,"14":0x41E0,"15":0x4DEC,
+            "16":0x502E,"17":0x547A,"18":0x5BAF,"19":0x5CB1,"20":0x5D9B,
+            "21":0x64E8,"22":0x6698,"23":0x6829,"24":0x73F3,"25":0x74DD,
+            "26":0x75C8,"27":0x7D9E,"28":0x7F88,"29":0x870E,"30":0x8FAE,
+            "31":0x9098,"32":0x9CF0
+        }
+    },
+    "arkanoidrevengeofdoh":{
+        "ROWS":18,
+        "COLS":13,
+        "DATAHANDLER": ROMWriter_ArkanoidRevengeOfDoh(),
+        "LEVELS":{
+            "1":0x593E,"2R":0x6676,"2L":0x68AA,"3R":0x5C8C,"3L":0x5DA6,
+            "4R":0x620E,"4L":0x9D8A,"5R":0x93A0,"5L":0x6328,"6R":0x9C70,"6L":0x6E2C,
+            "7R":0x7060,"7L":0x5EC0,"8R":0x5A58,"8L":0x6328,"9R":0x6F46,"9L":0x6CD2,
+            "10R":0x60F4,"10L":0x5FDA,"11R":0x7EB2,"11L":0x8782,"12R":0x7816,"12L":0x96EE,
+            "13R":0x8200,"13L":0x9922,"14R":0x9286,"14L":0x8434,"15R":0x94BA,"15L":0x7930,
+            "16R":0x69C4,"16L":0x6ADE,"18R":0x7C7E,"18L":0x7B64,"19R":0x89B6,"19L":0x8668,
+            "20R":0x7294,"20L":0x831A,"21R":0x6D12,"21L":0x8D04,"22R":0x8F38,"22L":0x6442,
+            "23R":0x7FCC,"23L":0x5B72,"24R":0x73AE,"24L":0x889C,"25R":0x9B56,"25L":0x76FC,
+            "26R":0x95D4,"26L":0x655C,"27R":0x8E1E,"27L":0x7D98,"28R":0x8AD0,"28L":0x75E2,
+            "29R":0x74C8,"29L":0x916C,"30R":0x854E,"30L":0x9A3C,"31R":0x80E6,"31L":0x9052,
+            "32R":0x9808,"32L":0x8BEA,"33R":0x717A,"33L":0x6790
+        }
+    }
+}
--- main.py
+++ main.py
@@ -15,90 +15,17 @@ import datahandler
 # ---------- TODOS
 #TODO: Classes and making this less...rigid.
 #TODO: Revenge of DOH has movable blocks. lets add a radio button for those
-#TODO: Handle writing across the 2 memory banks for the OG arkanoid. < 8000 gets ic17 otherwise use ic16.
-#0x0000-0x7FFF	32768	a75-19.ic17	CRC(d3ad37d7) (Fluke: 4189)
-#0x8000-0xFFFF	32768	a75-18.ic16	CRC(cdc08301) (Fluke: E951)
-# -- to find address simply remove 0x8000 from anything on ic16! (aka anything greater than 0x8000 for address)
 #TODO: A click menu instead of a drop down (think like paint!)
 window = tk.Tk()
 window.geometry("391x600")
 window.title("Artanoid")
 # ---------- Initialize
-#dirRoot        = path.dirname(path.realpath(__file__))
-#dirData        = dirRoot+"\\data"
-#lvlDataRom     = dirData+"\\arkanoidrevengeofdoh\\b08_13.3e"
 blnSaving      = False
 lstBrickData   = []
 strCurrentGame  = tk.StringVar()
 strCurrentBlock = tk.StringVar()
 strCurrentLevel = tk.StringVar()
-GAME_DATA = {
-    "arkanoid":{
-        "ROWS":18,
-        "COLS":13,
-        "DIRECTION":datahandler.BlockWriteDirection.LEFT_TO_RIGHT,
-        #"ROM":"",
-        #"CHECKSUM":{"location":None,"override":None},
-        "DATAHANDLER": datahandler.ROMWriter_Arkanoid(),
-        "BLOCKS":{
-            "clear"          : blocks.Clear(),
-            "white_flat"     : blocks.White_Flat(),
-            "red_flat"       : blocks.Red_Flat(),
-            "pink_flat"      : blocks.Pink_Flat(),
-            "orange_flat"    : blocks.Orange_Flat(),
-            "yellow_flat"    : blocks.Yellow_Flat(),
-            "green_flat"     : blocks.Green_Flat(),
-            "turqouise_flat" : blocks.Turqouise_Flat(),
-            "blue_flat"      : blocks.Blue_Flat(),
-            "grey_flat"      : blocks.Grey_Flat(),
-            "gold_flat"      : blocks.Gold_Flat(),
-        },
-        "LEVELS":{
-            "1":0xBF15,"2":0x0138,"3":0xBE2B,"4":0x04CB,"5":0x0FE0,
-            "6":0x14D8,"7":0x16C1,"8":0x1F46,"9":0x22CE,"10":0x2980,
-            "11":0x0A08,"12":0x3460,"13":0x40F6,"14":0x41E0,"15":0x4DEC,
-            "16":0x502E,"17":0x547A,"18":0x5BAF,"19":0x5CB1,"20":0x5D9B,
-            "21":0x64E8,"22":0x6698,"23":0x6829,"24":0x73F3,"25":0x74DD,
-            "26":0x75C8,"27":0x7D9E,"28":0x7F88,"29":0x870E,"30":0x8FAE,
-            "31":0x9098,"32":0x9CF0
-        }
-    },
-    "arkanoidrevengeofdoh":{
-        "ROWS":18,
-        "COLS":13,
-        "DIRECTION":datahandler.BlockWriteDirection.RIGHT_TO_LEFT,
-        #"ROM":"b08_13.3e",
-        #"CHECKSUM":{"location":0x0A99,"override":b'\xAF'},
-        "DATAHANDLER": datahandler.ROMWriter_ArkanoidRevengeOfDoh(),
-        "BLOCKS":{
-            "clear"          : blocks.Clear(),
-            "white_flat"     : blocks.White_Flat_AROD(),
-            "red_flat"       : blocks.Red_Flat_AROD(),
-            "pink_flat"      : blocks.Pink_Flat_AROD(),
-            "orange_flat"    : blocks.Orange_Flat_AROD(),
-            "yellow_flat"    : blocks.Yellow_Flat_AROD(),
-            "green_flat"     : blocks.Green_Flat_AROD(),
-            "turqouise_flat" : blocks.Turqouise_Flat_AROD(),
-            "blue_flat"      : blocks.Blue_Flat_AROD(),
-            "grey_1ridge"    : blocks.Grey_1Ridge_AROD(),
-            "gold_1ridge"    : blocks.Gold_1Ridge_AROD(),
-            "grey_2ridge"    : blocks.Grey_2Ridge_AROD()
-        },
-        "LEVELS":{
-            "1":0x593E,"2R":0x6676,"2L":0x68AA,"3R":0x5C8C,"3L":0x5DA6,
-            "4R":0x620E,"4L":0x9D8A,"5R":0x93A0,"5L":0x6328,"6R":0x9C70,"6L":0x6E2C,
-            "7R":0x7060,"7L":0x5EC0,"8R":0x5A58,"8L":0x6328,"9R":0x6F46,"9L":0x6CD2,
-            "10R":0x60F4,"10L":0x5FDA,"11R":0x7EB2,"11L":0x8782,"12R":0x7816,"12L":0x96EE,
-            "13R":0x8200,"13L":0x9922,"14R":0x9286,"14L":0x8434,"15R":0x94BA,"15L":0x7930,
-            "16R":0x69C4,"16L":0x6ADE,"18R":0x7C7E,"18L":0x7B64,"19R":0x89B6,"19L":0x8668,
-            "20R":0x7294,"20L":0x831A,"21R":0x6D12,"21L":0x8D04,"22R":0x8F38,"22L":0x6442,
-            "23R":0x7FCC,"23L":0x5B72,"24R":0x73AE,"24L":0x889C,"25R":0x9B56,"25L":0x76FC,
-            "26R":0x95D4,"26L":0x655C,"27R":0x8E1E,"27L":0x7D98,"28R":0x8AD0,"28L":0x75E2,
-            "29R":0x74C8,"29L":0x916C,"30R":0x854E,"30L":0x9A3C,"31R":0x80E6,"31L":0x9052,
-            "32R":0x9808,"32L":0x8BEA,"33R":0x717A,"33L":0x6790
-        }
-    }
-}
+
 
 # ----- Header
 frmHeader       = tk.Frame(window,height=20)
@@ -116,9 +43,9 @@ lblGameSelect.grid(row=0, column=0)
 
 def refreshKeys():
     global txtBlockSelect,txtLevelSelect
-    tempList = list(GAME_DATA[strCurrentGame.get()]["LEVELS"].keys())
+    tempList = list(datahandler.GAME_DATA[strCurrentGame.get()]["LEVELS"].keys())
     txtLevelSelect.config(values=tempList)
-    tempList = list(GAME_DATA[strCurrentGame.get()]["BLOCKS"].keys())
+    tempList = list(datahandler.GAME_DATA[strCurrentGame.get()]["DATAHANDLER"].blockset.keys())
     tempList.remove("clear")
     txtBlockSelect.config(values=tempList)
     initBrickData()
@@ -129,13 +56,13 @@ txtGameSelect.grid(row=0, column=1)
 # -- Level Select
 lblLevelSelect = tk.Label(frmControls,text="Level:")
 lblLevelSelect.grid(row=0, column=2)
-tempList = list(GAME_DATA[strCurrentGame.get()]["LEVELS"].keys())
+tempList = list(datahandler.GAME_DATA[strCurrentGame.get()]["LEVELS"].keys())
 txtLevelSelect = tk.Spinbox(frmControls,textvariable=strCurrentLevel,values=tempList,state='readonly')
 txtLevelSelect.grid(row=0, column=3)
 # - Block Select Control
 lblBlockSelect = tk.Label(frmControls,text="Block:")
 lblBlockSelect.grid(row=1, column=0)
-tempList = list(GAME_DATA[strCurrentGame.get()]["BLOCKS"].keys())
+tempList = list(datahandler.GAME_DATA[strCurrentGame.get()]["DATAHANDLER"].blockset.keys())
 tempList.remove("clear")
 txtBlockSelect = tk.Spinbox(frmControls,textvariable=strCurrentBlock,values=tempList,state='readonly')
 txtBlockSelect.grid(row=1, column=1)
@@ -162,131 +89,32 @@ def rightMouseEvent(event):
     print("erased Row: " +str(coords["row"])+" Col: "+str(coords["col"]))
     setBrickData(coords["row"],coords["col"],"clear")
 # ---------- Methods
-# TODO this is hot garbage and needs to be updated into a handler class
 def saveData():
-    #Maybe this doesn't need an update and I can consolidate....
-    global GAME_DATA,strCurrentGame
-    saveDataArkanoid()
+    lvlStart =  datahandler.GAME_DATA[strCurrentGame.get()]["LEVELS"][strCurrentLevel.get()]
+    rows     =  datahandler.GAME_DATA[strCurrentGame.get()]["ROWS"]
+    cols     =  datahandler.GAME_DATA[strCurrentGame.get()]["COLS"]
+    datahandler.GAME_DATA[strCurrentGame.get()]["DATAHANDLER"].save(lvlStart,rows,cols,lstBrickData)
         
-def saveDataArkanoid():
-    global GAME_DATA,strCurrentGame,strCurrentLevel,lstBrickData
-    dataHandler = GAME_DATA[strCurrentGame.get()]["DATAHANDLER"]
-    content     = []
-    lvlStart    = GAME_DATA[strCurrentGame.get()]["LEVELS"][strCurrentLevel.get()]
-    romFilePath = dataHandler.getROMFilePath(lvlStart)
-    #now adjust lvlStart!
-    lvlStart    = dataHandler.adjustROMAddressForLvlStart(lvlStart)
-    lvlStart    = int(lvlStart)
-    lvlEnd      = lvlStart+(GAME_DATA[strCurrentGame.get()]["COLS"] * GAME_DATA[strCurrentGame.get()]["ROWS"])
-    maxCols=GAME_DATA[strCurrentGame.get()]["COLS"]
-    romFilePath = dataHandler.getROMFilePath(lvlStart)
-
-    print("Reading Rom: %s" % romFilePath)
-    with open(romFilePath, "rb") as fr:
-        data = fr.read(1)
-        content=data
-        idx=0
-        row=-1
-        col=-1
-        while data:
-            data = fr.read(1)
-            idx+=1
-            checksumData = dataHandler.checkIfChecksum(idx,data)
-            if checksumData != data:
-                content+=checksumData
-            # TODO -- this is shifted in the dumbest way and I cannot figure it out.
-            elif idx >= lvlStart and idx < lvlEnd:
-                offset  = (idx-lvlStart)
-                cellRow = math.floor(offset/maxCols)
-                cellCol = math.floor(offset%maxCols)
-                cellIdx = (cellRow*maxCols)+(cellCol)
-                if GAME_DATA[strCurrentGame.get()]["DIRECTION"] == datahandler.BlockWriteDirection.RIGHT_TO_LEFT:
-                    cellIdx = (cellRow*maxCols)+(maxCols-cellCol)-1 # Because taito wanted to throw off hackers?
-                #print("Mem Idx: %d Cell Idx: %d, Row: %d Col: %d" % (offset,cellIdx,cellRow,cellCol))
-                #try:
-                romCode=GAME_DATA[strCurrentGame.get()]["BLOCKS"][lstBrickData[cellIdx]].getCode_Static()
-                content+=romCode
-                #except:
-                #    print("ERROR!"")
-                #    content+=data
-            else:
-                content+=data
-
-    print("Done Reading Rom")
-    print("Writing Rom: %s" % romFilePath)
-    if ".new" not in romFilePath: 
-        with open(romFilePath+".new", "wb") as fw:
-            fw.write(content)
-    else:
-         with open(romFilePath, "wb") as fw:
-             fw.write(content)
-    print("Done Writing Rom")
-
-#IM NOT READY TO QUIT YOU!!!
-#def saveDataRevengeOfDoh():
-#    global GAME_DATA,strCurrentGame,strCurrentLevel,lstBrickData
-#    content = []
-#    lvlDataRom = dirData+"\\"+strCurrentGame.get()+"\\"+GAME_DATA[strCurrentGame.get()]["ROM"]
-#    if path.exists(lvlDataRom+".new"):
-#        lvlDataRom = lvlDataRom+".new"
-#    with open(lvlDataRom, "rb") as fr:
-#        data = fr.read(1)
-#        content=data
-#        idx=0
-#        lvlStart=int(GAME_DATA[strCurrentGame.get()]["LEVELS"][strCurrentLevel.get()])-1
-#        while data:
-#            data = fr.read(1)
-#            if GAME_DATA[strCurrentGame.get()]["CHECKSUM"]["location"] != None and idx == int(GAME_DATA[strCurrentGame.get()]["CHECKSUM"]["location"])-1:
-#                content+=GAME_DATA[strCurrentGame.get()]["CHECKSUM"]["override"]
-#            elif idx >= lvlStart and idx < lvlStart+(GAME_DATA[strCurrentGame.get()]["COLS"] * GAME_DATA[strCurrentGame.get()]["ROWS"]):
-#                offset=(idx-lvlStart)
-#                maxCols=GAME_DATA[strCurrentGame.get()]["COLS"]
-#                cellRow=math.ceil(offset/maxCols)-1
-#                if cellRow < 0:
-#                    cellRow = 0
-#                cellCol=math.ceil(offset%maxCols)
-#                cellIdx=(cellRow*maxCols)+(cellCol)-1 # Left right by default
-#                if GAME_DATA[strCurrentGame.get()]["DIRECTION"] == "rightleft":
-#                    cellIdx=(cellRow*maxCols)+(maxCols-cellCol)-1 # Because taito wanted to throw off hackers?
-#                #try:
-#                romCode=GAME_DATA[strCurrentGame.get()]["BLOCKS"][lstBrickData[cellIdx]].getCode_Static()
-#                content+=romCode
-#                #except:
-#                #    print("Col: %d Row %d generated Index: %d" % (cellCol,cellRow,cellIdx))
-#                #    content+=data
-#            else:
-#                content+=data
-#            idx+=1
-#    print("done reading")
-#    if ".new" not in lvlDataRom: 
-#        with open(lvlDataRom+".new", "wb") as fw:
-#            fw.write(content)
-#    else:
-#        with open(lvlDataRom, "wb") as fw:
-#            fw.write(content)
-#    print("done writing")
-
-
 def setBrickData(row,col,color):
     global lstBrickData
-    lstBrickData[(row*GAME_DATA[strCurrentGame.get()]["COLS"])+col] = color
+    lstBrickData[(row*datahandler.GAME_DATA[strCurrentGame.get()]["COLS"])+col] = color
     drawBricks()
 
 def initBrickData():
     global lstBrickData
     if len(lstBrickData) > 0:
         lstBrickData=[]
-    for row in range(GAME_DATA[strCurrentGame.get()]["ROWS"]):
-        for col in range(GAME_DATA[strCurrentGame.get()]["COLS"]):
+    for row in range(datahandler.GAME_DATA[strCurrentGame.get()]["ROWS"]):
+        for col in range(datahandler.GAME_DATA[strCurrentGame.get()]["COLS"]):
             lstBrickData.append("clear")
 
 def drawBricks():
     global lstBrickData,cnvDisplay
     #TODO -- constants
-    for row in range(GAME_DATA[strCurrentGame.get()]["ROWS"]):
-        for col in range(GAME_DATA[strCurrentGame.get()]["COLS"]):
-            colorIdx = lstBrickData[(row*GAME_DATA[strCurrentGame.get()]["COLS"])+col]
-            GAME_DATA[strCurrentGame.get()]["BLOCKS"][colorIdx].draw(cnvDisplay,row,col)
+    for row in range(datahandler.GAME_DATA[strCurrentGame.get()]["ROWS"]):
+        for col in range(datahandler.GAME_DATA[strCurrentGame.get()]["COLS"]):
+            colorIdx = lstBrickData[(row*datahandler.GAME_DATA[strCurrentGame.get()]["COLS"])+col]
+            datahandler.GAME_DATA[strCurrentGame.get()]["DATAHANDLER"].blockset[colorIdx].draw(cnvDisplay,row,col)
 
 # ---------- Main
 if __name__ == '__main__':
@@ -300,7 +128,6 @@ if __name__ == '__main__':
 
     #
     window.update()
-    #window.after(0, updateLoop)
     window.mainloop()
 
 
